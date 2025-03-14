3d41086948758595c8a3c30c72fb59f271ecb5e8
suzukart <artbuscus73@gmail.com>
2025-03-13 21:06:35 -0400

    run.py hotfix

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app
+import os
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app = create_app()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
