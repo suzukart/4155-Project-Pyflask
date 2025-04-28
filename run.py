bc5e4cebb1c6f71fa67e46173a18049007f3e096
suzukart <artbuscus73@gmail.com>
2025-04-27 20:01:30 -0400

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8081))
     app = create_app()
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
